0affdad7ec94a5b67f2b1dd82caf21a0ea02596f
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2022-01-04 21:25:14 +0530

    Update index.js

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -8,22 +8,22 @@ export default function Socialicons(params) {
     <div className="stick_follow_icon">
       <ul>
         <li>
-          <a href={socialprofils.twitter}>
+          <a href={socialprofils.twitter} rel='noopener noreferrer' target='_blank'>
             <FaTwitter />
           </a>
         </li>
         <li>
-          <a href={socialprofils.github}>
+          <a href={socialprofils.github} rel='noopener noreferrer' target='_blank'>
             <FaGithub />
           </a>
         </li>
         <li>
-          <a href={socialprofils.facebook}>
+          <a href={socialprofils.facebook} rel='noopener noreferrer' target='_blank'>
             <FaFacebookF />
           </a>
         </li>
         <li>
-          <a href={socialprofils.linkedin}>
+          <a href={socialprofils.linkedin} rel='noopener noreferrer' target='_blank'>
             <FaLinkedin />
           </a>
         </li>
